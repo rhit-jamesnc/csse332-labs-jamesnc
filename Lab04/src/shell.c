010762b6ad3281cc8da893f4c4476fe1f8f7c916
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-04-28 19:39:59 -0400

    Working on Lab04

--- Lab04/src/shell.c
+++ Lab04/src/shell.c
@@ -85,20 +85,24 @@ generate_exec_args(char *cmd, char *argv[])
   // TODO:
   // =====
   //  Implement this function...
-  int count = 0;
-  char *token;
 
-  token = strtok(cmd, " ");
+  char *curr = cmd;
+  argv[0] = cmd;
 
-  while (token != NULL && count < MAX_ARGS - 1) {
-      argv[count] = token;
-      count++;
-      token = strtok(NULL, " ");
+  int i = 1;
+  while(*curr != '\0') {
+
+    if(isspace(*curr)) {
+      *curr = '\0';
+
+      if(*(curr+1) != '\0') {
+        argv[i++] = curr + 1;
+      }
+    }
+    curr++;
   }
-
-  argv[count] = NULL;
-
-  return count;
+  argv[i] = NULL;
+  return i;
 }
 
 int
@@ -107,7 +111,57 @@ start_fg_command(char *cmd)
   // TODO:
   // =====
   //   Implement code to start a foreground command.
-  return -1;
+  char *argv[MAX_ARGS];
+  
+  // Clean implementation of parsing to ensure argv[0] is correct
+  int count = 0;
+  char *token = strtok(cmd, " ");
+  while (token != NULL && count < MAX_ARGS - 1) {
+    argv[count++] = token;
+    token = strtok(NULL, " ");
+  }
+  argv[count] = NULL;
+
+  if (count <= 0) {
+    return -1;
+  }
+
+  // Printing exactly as requested in the test logs
+  printf("Running forgeround command %s with %d arguments\n", argv[0], count);
+
+  pid_t pid = fork();
+
+  if (pid < 0) {
+    perror("fork");
+    return -1;
+  }
+
+  if (pid == 0) {
+    // Child Process: Execute the command
+    execvp(argv[0], argv);
+    
+    // If execvp returns, it failed
+    perror("execvp");
+    exit(EXIT_FAILURE);
+  } else {
+    // Parent Process: Wait for the child to finish
+    int status;
+    waitpid(pid, &status, 0);
+
+    if (WIFEXITED(status)) {
+      int exit_code = WEXITSTATUS(status);
+      
+      // Check if it exited successfully (exit code 0)
+      if (exit_code == 0) {
+        printf("Command %s exited successfully\n", argv[0]);
+      } else {
+        printf("Command %s exited with code %d\n", argv[0], exit_code);
+      }
+      return exit_code;
+    }
+
+    return -1;
+  }
 }
 
 void
@@ -116,4 +170,41 @@ start_bg_command(char *cmd)
   // TODO:
   // =====
   //   Implement code to start a background command.
+  char *argv[MAX_ARGS];
+  
+  int len = strlen(cmd);
+  cmd[len] = '&';
+  cmd[len+1] = '\0';
+
+  int count = generate_exec_args(cmd, argv);
+
+  if (count <= 0) {
+    return;
+  }
+
+  pid_t pid = fork();
+
+  if (pid < 0) {
+    perror("fork");
+    return;
+  }
+
+  if (pid == 0) {
+    printf("Running background command %s with %d arguments\n", argv[0], count);
+    fflush(stdout);
+
+    for (int i = 0; i < count; i++) {
+      if (argv[i] != NULL && strcmp(argv[i], "&") == 0) {
+        argv[i] = NULL;
+        break;
+      }
+    }
+
+    if (execvp(argv[0], argv) == -1) {
+      perror("execvp");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    return;
+  }
 }
